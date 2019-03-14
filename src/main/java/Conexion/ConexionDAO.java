c8d382f00442319184e084f882548f4ba1f42306
Felipe <gonzalezfelipe784@gmail.com>
2019-03-14 13:56:12 -0500

    Maven

--- src/main/java/Conexion/ConexionDAO.java
+++ src/main/java/Conexion/ConexionDAO.java
@@ -14,12 +14,20 @@ import java.sql.DriverManager;
  */
 public class ConexionDAO {
 
+    public static void main(String[] args) {
+        try {
+            Connection dao = ConexionDAO.GetConnection();
+            dao.close();
+        } catch (Exception e) {
+        }
+    }
+
     public static Connection GetConnection() {
         Connection conexion = null;
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
 //          conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/Clase", "root", "root"); //DRIVER MYSQL
-            conexion = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "CLASE", "CLASE");
+            conexion = DriverManager.getConnection("jdbc:oracle:thin:@192.190.43.117:1521:ORCL", "CLASE", "CLASE");
         } catch (Exception e) {
             e.printStackTrace();
         }
